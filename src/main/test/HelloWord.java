940e7966bb080a4215f56ac762ae0fccbdcf7beb
John Doe <john@sample.com>
2019-11-01 18:32:05 +0800

    initial commit

--- src/main/test/HelloWord.java
+++ src/main/test/HelloWord.java
@@ -1,8 +1,16 @@
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 /**
  * xx模块
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("hello,word");
+        HSSFWorkbook workbook= new HSSFWorkbook();
+        HSSFSheet sheet=workbook.createSheet();
+        HSSFRow row=sheet.createRow(0);
+
+
     }
 }
